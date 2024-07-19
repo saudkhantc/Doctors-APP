fba2e053586c6ce35721018f2c7cd2ca2fa26826
saud khan <saudkhantc@gmail.com>
2024-07-18 17:12:53 -0700

    merg the code and redesign the navbar and landingpage

--- src/app/components/filterDoctors/FilterDoctors.jsx
+++ src/app/components/filterDoctors/FilterDoctors.jsx
@@ -1,88 +1,88 @@
-import Image from "next/image";
-import img1 from "../../../assets/images/landimage.jpg";
-import { FaHeart } from "react-icons/fa";
-import { LuMapPin } from "react-icons/lu";
-import { GrUserExpert } from "react-icons/gr";
-import { CiStar } from "react-icons/ci";
-import { IoMdTime } from "react-icons/io";
-import { SlLike } from "react-icons/sl";
-import CustomButton from "../customButton/CustomBotton";
+// import Image from "next/image";
+// import img1 from "../../../assets/images/landimage.jpg";
+// import { FaHeart } from "react-icons/fa";
+// import { LuMapPin } from "react-icons/lu";
+// import { GrUserExpert } from "react-icons/gr";
+// import { CiStar } from "react-icons/ci";
+// import { IoMdTime } from "react-icons/io";
+// import { SlLike } from "react-icons/sl";
+// import CustomButton from "../customButton/CustomBotton";
 
-const FilterDoctors=()=>{
-    return(
-        <>
-        <div className="flex border border-red-400 justify-between p-20">
-        <div className="lft flex gap-5">
-        <div className="w-[200px] h-[150px] bg-cover rounded-md " style={{ backgroundImage: `url(${img1.src})` }}>
-        <div className="flex justify-end p-4">
-        <FaHeart color="gray"/>
-        </div>
+// const FilterDoctors=()=>{
+//     return(
+//         <>
+//         <div className="flex border border-red-400 justify-between p-20">
+//         <div className="lft flex gap-5">
+//         <div className="w-[200px] h-[150px] bg-cover rounded-md " style={{ backgroundImage: `url(${img1.src})` }}>
+//         <div className="flex justify-end p-4">
+//         <FaHeart color="gray"/>
+//         </div>
      
-        </div>
+//         </div>
 
-        <div className="flex flex-col gap-4">
-        <div className="flex flex-col ">
-        <h1 className="text-xl font-semibold">Dr. Darren Elder</h1>
-        <span className="text-gray-500 text-sm">BDS, MDS - Oral & Maxillofacial Surgery</span>
-        </div>
-        <div className="flex flex-col gap-4">
-        <div className="flex gap-2 items-center">
-        <LuMapPin />
-        <h1 className="font-semibold text-gray-500">1.1 mi - Georgia USA <span className="text-blue-600 ml-2">Get Direction</span></h1>
+//         <div className="flex flex-col gap-4">
+//         <div className="flex flex-col ">
+//         <h1 className="text-xl font-semibold">Dr. Darren Elder</h1>
+//         <span className="text-gray-500 text-sm">BDS, MDS - Oral & Maxillofacial Surgery</span>
+//         </div>
+//         <div className="flex flex-col gap-4">
+//         <div className="flex gap-2 items-center">
+//         <LuMapPin />
+//         <h1 className="font-semibold text-gray-500">1.1 mi - Georgia USA <span className="text-blue-600 ml-2">Get Direction</span></h1>
 
-        </div>
-        <div className="flex gap-2 items-center">
-        <GrUserExpert />
-        <h1 className="font-semibold text-gray-500">15 Years of Experience</h1>
-        </div>
-        </div>
+//         </div>
+//         <div className="flex gap-2 items-center">
+//         <GrUserExpert />
+//         <h1 className="font-semibold text-gray-500">15 Years of Experience</h1>
+//         </div>
+//         </div>
 
-        <div className="flex gap-2 items-center">
-        <div className="flex items-center gap-2 bg-yellow-400 py-1 px-2 rounded-md">
-       <CiStar size={20} /> 
-       <span className="text-white font-medium">4.4</span>
-       </div>
-       <div>
-            <span className="text-gray-500">(22 Reviews)</span>
-            </div>
-        </div>
+//         <div className="flex gap-2 items-center">
+//         <div className="flex items-center gap-2 bg-yellow-400 py-1 px-2 rounded-md">
+//        <CiStar size={20} /> 
+//        <span className="text-white font-medium">4.4</span>
+//        </div>
+//        <div>
+//             <span className="text-gray-500">(22 Reviews)</span>
+//             </div>
+//         </div>
 
-<div>
-</div>
-        </div>
-        </div>
-
-
-        <div className="rght flex flex-col gap-3">
-        <div className="flex gap-2 items-center ">
-        <IoMdTime />
-        <span className="bg-yellow-100 text-yellow-500 p-2 rounded-md">Available Tomorrow</span>
-        </div>
-
-        <div className="flex gap-2 items-center ">
-        <SlLike />
-        <span className="text-gray-500 font-light">90% (380 Votes)</span>
-        </div>
-
-        <div className="flex gap-2 items-center ">
-            <span>$ $2800</span>
-        </div>
-
-        <div>
-            <CustomButton buttonText="Book Appointment" />
-        </div>
-
-        <div>
-        <CustomButton buttonText="Book Online Consultation" />
-        </div>
+// <div>
+// </div>
+//         </div>
+//         </div>
 
 
+//         <div className="rght flex flex-col gap-3">
+//         <div className="flex gap-2 items-center ">
+//         <IoMdTime />
+//         <span className="bg-yellow-100 text-yellow-500 p-2 rounded-md">Available Tomorrow</span>
+//         </div>
 
-        </div>
+//         <div className="flex gap-2 items-center ">
+//         <SlLike />
+//         <span className="text-gray-500 font-light">90% (380 Votes)</span>
+//         </div>
 
-        </div>
+//         <div className="flex gap-2 items-center ">
+//             <span>$ $2800</span>
+//         </div>
 
-        </>
-    )
-};
-export default FilterDoctors;
+//         <div>
+//             <CustomButton buttonText="Book Appointment" />
+//         </div>
+
+//         <div>
+//         <CustomButton buttonText="Book Online Consultation" />
+//         </div>
+
+
+
+//         </div>
+
+//         </div>
+
+//         </>
+//     )
+// };
+// export default FilterDoctors;
